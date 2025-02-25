1e50fb6a512e1511fc1936db2ba5fc09fe08a144
JonelBriones <jonel.c.briones@gmail.com>
2025-02-25 14:41:35 -0800

    customized login

--- components/spotify/Login.tsx
+++ components/spotify/Login.tsx
@@ -16,35 +16,78 @@ const Login = () => {
   }, []);
 
   if (providers == null) return <div>loading...</div>;
+
   return (
     <div className="h-screen w-screen place-items-center flex flex-col justify-center">
-      <div className="backgroundContainer h-[620px] w-[500px] flex  justify-between flex-col place-items-center ">
-        <div className="mt-20">
-          <Image
-            src={
-              "/images/2024 Spotify Brand Assets/Spotify_Full_Logo_RGB_White.png"
-            }
-            alt="Spotify_Full_Logo_RGB_White.png"
-            width={180}
-            height={180}
-          />
+      <div className="backgroundContainer h-[620px] w-[600px] flex flex-col place-items-center">
+        <div className="flex-col flex-1 flex place-items-center w-full">
+          <div className="flex-col place-items-center ">
+            <Image
+              src={
+                "/images/2024 Spotify Brand Assets/Spotify_Primary_Logo_RGB_White.png"
+              }
+              alt="Spotify_Full_Logo_RGB_White.png"
+              width={40}
+              height={40}
+            />
+            <h1 className="text-white text-[1.875rem] font-bold">
+              Log in to Spotify
+            </h1>
+          </div>
+          <div className="flex flex-col gap-2 place-items-center mt-4">
+            {/* {providers &&
+            Object.values(providers).map(
+              (provider) =>
+                provider.name !== "Spotify" && (
+                  <button
+                    key={provider.name}
+                    onClick={() =>
+                      signIn(provider.id, {
+                        callbackUrl: "/",
+                      })
+                    }
+                    className="text-white bg-[#1ed966] rounded-full p-2 w-full"
+                  >
+                    LOG IN TO {provider.name.toUpperCase()}
+                  </button>
+                )
+            )} */}
+
+            {Array(3)
+              .fill(null)
+              .map((_, idx) => (
+                <button
+                  key={idx}
+                  className="text-white font-semibold bg-transparent rounded-full p-2 w-[330px] border border-neutral-700 hover:border-white mb-[.5rem]"
+                >
+                  Log in
+                </button>
+              ))}
+          </div>
         </div>
 
-        <div className="flex flex-1 place-items-center w-full">
-          {providers &&
-            Object.values(providers).map((provider) => (
-              <button
-                key={provider.name}
-                onClick={() =>
-                  signIn(provider.id, {
-                    callbackUrl: "/",
-                  })
-                }
-                className="text-white bg-[#1ed966] rounded-full p-4 w-full text-[1.2em]"
-              >
-                LOG IN TO {provider.name.toUpperCase()}
-              </button>
-            ))}
+        <div className="border-b w-full  border-neutral-700" />
+
+        <div className="flex place-items-center w-full flex-1">
+          <div className="flex place-items-center justify-center flex-1">
+            {providers &&
+              Object.values(providers).map(
+                (provider) =>
+                  provider.name == "Spotify" && (
+                    <button
+                      key={provider.name}
+                      onClick={() =>
+                        signIn(provider.id, {
+                          callbackUrl: "/",
+                        })
+                      }
+                      className="text-black font-semibold bg-[#1ed760] rounded-full p-2 w-[330px] hover:w-[340px] hover:py-[.575rem] hover:bg-[#1ef984]"
+                    >
+                      Log In
+                    </button>
+                  )
+              )}
+          </div>
         </div>
       </div>
     </div>
